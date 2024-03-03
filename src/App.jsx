8bc2bce6fd018722da2a805a4ced9cb9e3c9e5aa
Enayet <enayetflweb@gamil.com>
2024-03-03 20:48:32 +0600

    max width added in the header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import ButtonGradient from "./assets/svg/ButtonGradient"
-import Button from "./components/Button"
-import Header from "./components/Header"
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden ">
-      <Header/>
-      <Button className='text-base font-sans '>Resume</Button>
-    </div>
-    <ButtonGradient/>
+      <div className=" pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden ">
+        
+          <Header />
+       
+        <Button className="text-base font-sans ">Resume</Button>
+      </div>
+      <ButtonGradient />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
