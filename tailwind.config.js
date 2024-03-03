156b6cf5826e9a79a34972a4f90627570328e391
Enayet <enayetflweb@gamil.com>
2024-03-03 16:01:53 +0600

    index.css file edited and font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,8 +40,8 @@ export default {
       },
       fontFamily: {
         sans: ["var(--font-sora)", ...fontFamily.sans],
-        code: "var(--font-code)",
-        grotesk: "var(--font-grotesk)",
+        code: "var(--font-madimi)",
+        grotesk: "var(--font-bebas)",
       },
       letterSpacing: {
         tagline: ".15em",
